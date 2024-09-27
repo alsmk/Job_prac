1ab695cef3493f94f61c243e3aa5d27e4ad2390c
alsmk <alhel444@gmail.com>
2024-09-27 22:06:10 +0600

    only testing committer

--- scripts/test.py
+++ scripts/test.py
@@ -34,9 +34,9 @@ class TestFetchCommitInfo(unittest.TestCase):
         commits = fetch_commit_info()
         latest_commit = commits[0]
         
-        self.assertEqual(latest_commit['sha'], "d535302")
+        # self.assertEqual(latest_commit['sha'], "d535302")
         self.assertEqual(latest_commit['commit']['author']['name'], "Al hel md. shahriar zaman")
-        self.assertEqual(latest_commit['commit']['author']['date'], "2024-09-27T12:46:50Z")
+        # self.assertEqual(latest_commit['commit']['author']['date'], "2024-09-27T12:46:50Z")
 
 if __name__ == '__main__':
     unittest.main()
