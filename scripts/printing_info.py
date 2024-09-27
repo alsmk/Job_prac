768a8fdda7cbe263824b532313403782df9e587a
alsmk <alhel444@gmail.com>
2024-09-27 19:01:08 +0600

    venv activated

---
+++ scripts/printing_info.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+
+
+def fetch_commit_info():
+    headers = {
+        'Authorization': f'token {token}'
+    }
+    response = requests.get(f'https://api.github.com/repos/{repo}/commits', headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def print_commit_info():
+    commit_info = fetch_commit_info()
+    for commit in commit_info:
+        print(f'Commit: {commit["sha"][:7]}')
+        print(f'Author: {commit["commit"]["author"]["name"]}')
+        print(f'Date: {commit["commit"]["author"]["date"]}')
+        print('----------------------------------------')
+    print(f'Total commits: {len(commit_info)}')
+
+
+
+
+if __name__ == '__main__':
+    print_commit_info()
+    
